5c7f1708accd1d7984e989c9c1808c89ca0bd895
Angel Pineda <lamostace@outlook.com>
2023-03-20 22:09:58 -0600

    Actualizacion de RR

--- src/control/RounRobin.java
+++ src/control/RounRobin.java
@@ -1,21 +1,25 @@
 package control;
+
 import java.util.ArrayList;
 import model.proceso;
+
 public class RounRobin {
+    ArrayList<proceso> arregloProceso;
+    public RounRobin() {
+    };// Constructor
 
-    public RounRobin();//Constructor
-
-    //Metodo para implementar el planificador
-    public ArrayList<String> RR(proceso[] arregloProceso) {
-        String arregloParaLUE = new ArrayList<String>(); //Arreglo a devolver para la tabla LUE
+    // Metodo para implementar el planificador
+    public ArrayList<String> RR(ArrayList<proceso> arregloProceso) {
+        this. arregloProceso = arregloProceso;
+        ArrayList<String> arregloParaLUE = new ArrayList<String>(); //Arreglo a devolver para la tabla LUE
         ordenarProcesos(arregloProceso); //Para ordenar el arreglo
-        int empiezaArreglo = arregloProceso[0].getTiempoLlegada();//Para ver si este no esta vacio al principio
-        int arrRafaga[arregloProceso.size()],i,j;
-        for (i=0;i<arrRafaga;i++){//Para poner en el arreglo a imprimir un espacio en blanco para saber que ahi no va nada.
-            arregloParaLUE[i]=" ";
+        int empiezaArreglo = arregloProceso.get(0).getTiempoLLegada();//Para ver si este no esta vacio al principio
+        int arrRafaga[] = new int[arregloProceso.size()],i,j;
+        for (i=0;i < arrRafaga.length ;i++){//Para poner en el arreglo a imprimir un espacio en blanco para saber que ahi no va nada.
+            arregloParaLUE.set(i, " ");
         }
         for (i = 0; i < arregloProceso.size(); i++) {//Para manejar la rafaga y saber si aun hay rafaga o no;
-            arrRafaga[i] = arregloProceso[i].getRafaga;
+            arrRafaga[i] = arregloProceso.get(i).getRafaga();
         }
         int aux=0,aux2=arregloProceso.size();
         do {
@@ -23,21 +27,21 @@ public class RounRobin {
                 aux=0;
             }else{
                 if (arrRafaga[aux]>0){
-                    if (arrRafaga[aux]==arregloProceso[aux].getRafaga()){
-                        arregloProceso[aux].setTiempoLLegada(empiezaArreglo);
+                    if (arrRafaga[aux]==arregloProceso.get(aux).getRafaga()){
+                        arregloProceso.get(aux).setTiempoArranque(empiezaArreglo);
                     }
-                    if (arrRafaga>=4){
+                    if (arrRafaga[aux] >= 4){
                         for (j=0;j<4;j++){
-                            arregloParaLUE.add(arregloProceso[aux].getId());
+                            arregloParaLUE.add(arregloProceso.get(aux).getId());
                         }
                         empiezaArreglo+=4;
                         arrRafaga[aux]-=4;
                     }else{
                         for (j=0;j<arrRafaga[aux];j++){
-                            arregloParaLUE.add(arregloProceso[aux].getId());
+                            arregloParaLUE.add(arregloProceso.get(aux).getId());
                         }
-                        empiezaArreglo+=arrRafaga[aux]:
-                        arregloProceso[aux].setTiempoFinalizacion(empiezaArreglo);
+                        empiezaArreglo+=arrRafaga[aux];
+                        arregloProceso.get(aux).setTiempoFinalizacion(empiezaArreglo);
                         arrRafaga[aux]=0;
                     }
                 }
@@ -47,18 +51,17 @@ public class RounRobin {
         return arregloParaLUE;
     }
 
-    public void ordenarProcesos(proceso[] arregloProceso) {
-        proceso aux = new proceso();
+    public void ordenarProcesos(ArrayList<proceso> arregloProceso) {
+        proceso aux;
         for (int i = 0; i < arregloProceso.size(); i++) {
             for (int j = 0; j < arregloProceso.size(); j++) {
-                if (arregloProceso[i].getTiempoLlegada() < arregloProceso[j].getTiempoLlegada()) {
-                    aux = arregloProceso[i];
-                    arregloProceso[i] = arregloProceso[j];
-                    arregloProceso[j] = aux;
+                if (arregloProceso.get(i).getTiempoLLegada() < arregloProceso.get(i).getTiempoLLegada()) {
+                    aux = arregloProceso.get(i);
+                    arregloProceso.set(i,arregloProceso.get(j));
+                    arregloProceso.set(j,aux);
                 }
             }
         }
     }
-
 
 }
