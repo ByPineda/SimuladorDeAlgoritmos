236058b8e5959509428e542777d73474f2a90600
Angel Pineda <lamostace@outlook.com>
2023-03-30 08:35:02 -0600

    Final Update

--- src/test/App.java
+++
@@ -1,8 +0,0 @@
-import view.PlanificadorProcesosGUI;
-
-public class App {
-    public static void main(String[] args) {
-        PlanificadorProcesosGUI gui = new PlanificadorProcesosGUI();
-        gui.setVisible(true);
-    }
-}
